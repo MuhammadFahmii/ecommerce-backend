eaef98f9306da45723f3b358de1411ea7ffcd068
Ahmad Ilman Fadilah <cst_dev42@unitedtractors.com>
2022-09-09 15:02:18 +0700

    :art: remove whitespace

--- src/Application/TodoLists/Queries/GetTodos/GetTodosQuery.cs
+++ src/Application/TodoLists/Queries/GetTodos/GetTodosQuery.cs
@@ -17,63 +17,64 @@ using netca.Application.Common.Vms;
 using netca.Domain.Enums;
 using Newtonsoft.Json.Linq;
 
-namespace netca.Application.TodoLists.Queries.GetTodos
+namespace netca.Application.TodoLists.Queries.GetTodos;
+
+/// <summary>
+/// GetTodosQuery
+/// </summary>
+[RetryPolicy(RetryCount = 2, SleepDuration = 500)]
+public class GetTodosQuery : QueryModel, IRequest<DocumentRootJson<List<TodoListVm>>>
 {
+}
+
+/// <summary>
+/// GetTodosQueryHandler
+/// </summary>
+public class GetTodosQueryHandler : IRequestHandler<GetTodosQuery, DocumentRootJson<List<TodoListVm>>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
     /// <summary>
-    /// GetTodosQuery
+    /// Initializes a new instance of the <see cref="GetTodosQueryHandler"/> class.
     /// </summary>
-    [RetryPolicy(RetryCount = 2, SleepDuration = 500)]
-    public class GetTodosQuery : QueryModel, IRequest<DocumentRootJson<List<TodoListVm>>>
+    /// <param name="context"></param>
+    /// <param name="mapper"></param>
+    public GetTodosQueryHandler(IApplicationDbContext context, IMapper mapper)
     {
+        _context = context;
+        _mapper = mapper;
     }
 
     /// <summary>
-    /// GetTodosQueryHandler
+    /// Handle
     /// </summary>
-    public class GetTodosQueryHandler : IRequestHandler<GetTodosQuery, DocumentRootJson<List<TodoListVm>>>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<DocumentRootJson<List<TodoListVm>>> Handle(GetTodosQuery request,
+        CancellationToken cancellationToken)
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="GetTodosQueryHandler"/> class.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="mapper"></param>
-        public GetTodosQueryHandler(IApplicationDbContext context, IMapper mapper)
+        var countEntity = await _context.TodoLists
+            .AsNoTracking()
+            .QueryWithoutLimit(request)
+            .Select(x => x.Id)
+            .CountAsync(cancellationToken);
+        var priorityLevels = Enum.GetValues(typeof(PriorityLevel))
+            .Cast<PriorityLevel>()
+            .Select(p => new PriorityLevelVm { Value = (int)p, Name = p.ToString() })
+            .ToList();
+        var data = await _context.TodoLists
+            .AsNoTracking()
+            .Query(request)
+            .ProjectTo<TodoListVm>(_mapper.ConfigurationProvider)
+            .OrderBy(t => t.Title).ToListAsync(cancellationToken);
+        var meta = new Meta
         {
-            _context = context;
-            _mapper = mapper;
-        }
+            { "priorityLevels", JToken.FromObject(priorityLevels) }
+        };
 
-        /// <summary>
-        /// Handle
-        /// </summary>
-        /// <param name="request"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public async Task<DocumentRootJson<List<TodoListVm>>> Handle(GetTodosQuery request, CancellationToken cancellationToken)
-        {
-            var countEntity = await _context.TodoLists
-                .AsNoTracking()
-                .QueryWithoutLimit(request)
-                .Select(x => x.Id)
-                .CountAsync(cancellationToken);
-            var priorityLevels = Enum.GetValues(typeof(PriorityLevel))
-                .Cast<PriorityLevel>()
-                .Select(p => new PriorityLevelVm { Value = (int)p, Name = p.ToString() })
-                .ToList();
-            var data = await _context.TodoLists
-                .AsNoTracking()
-                .Query(request)
-                .ProjectTo<TodoListVm>(_mapper.ConfigurationProvider)
-                .OrderBy(t => t.Title).ToListAsync(cancellationToken);
-            var meta = new Meta
-            {
-                { "priorityLevels", JToken.FromObject(priorityLevels)}
-            };
-
-            return JsonApiExtensions.ToJsonApiPaginated(data, meta, countEntity, request.PageNumber ?? Constants.DefaultPageNumber, request.PageSize ?? Constants.DefaultPageSize);
-        }
+        return JsonApiExtensions.ToJsonApiPaginated(data, meta, countEntity,
+            request.PageNumber ?? Constants.DefaultPageNumber, request.PageSize ?? Constants.DefaultPageSize);
     }
 }
