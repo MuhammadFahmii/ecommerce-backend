e9b943d315d9a14172fe40b612920f88b98e57a1
Ahmad Ilman Fadilah <cst_dev42@unitedtractors.com>
2022-09-17 12:55:39 +0700

    :recycle: add data todolist test #94762

---
+++ tests/Application.IntegrationTests/Data/TodoListDataTest.cs
@@ -0,0 +1,40 @@
+// ------------------------------------------------------------------------------------
+// TodoListDataTest.cs  2022
+// Copyright Ahmad Ilman Fadilah. All rights reserved.
+// ahmadilmanfadilah@gmail.com,ahmadilmanfadilah@outlook.com
+// -----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace netca.Application.IntegrationTests.Data;
+
+/// <summary>
+/// TodoListDataTest
+/// </summary>
+public class TodoListDataTest
+{
+    /// <summary>
+    /// ShouldRequireMinimumFields
+    /// </summary>
+    public static IEnumerable<TestCaseData> ShouldRequireMinimumFields
+    {
+        get
+        {
+            yield return new TestCaseData(null, "Title");
+            yield return new TestCaseData(Guid.NewGuid());
+        }
+    }
+    
+    /// <summary>
+    /// ShouldCreated
+    /// </summary>
+    public static IEnumerable<TestCaseData> ShouldCreated
+    {
+        get
+        {
+            yield return new TestCaseData(Guid.NewGuid(), "Shopping");
+        }
+    }
+}
